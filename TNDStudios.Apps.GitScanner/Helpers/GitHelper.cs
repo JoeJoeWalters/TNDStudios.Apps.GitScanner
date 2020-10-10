e93617e1d412a9fdb2b2cdc5208633c0842a33e4
TNDStudios <info@thenakeddeveloper.com>
2020-10-10 01:00:32 +0100

    Initial Commit with project set up

---
+++ TNDStudios.Apps.GitScanner/Helpers/GitHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibGit2Sharp;
+
+namespace TNDStudios.Apps.GitScanner.Helpers
+{
+    public interface IGitHelper
+    {
+        void Connect(string userName, string password);
+        void Clone(string remote, string localPath);
+    }
+
+    /// <summary>
+    /// https://edi.wang/post/2019/3/26/operate-git-with-net-core
+    /// https://github.com/libgit2/libgit2sharp/
+    /// </summary>
+    public class Lib2GitHelper : IGitHelper
+    {
+        private UsernamePasswordCredentials _credentials;
+
+        public Lib2GitHelper()
+        {
+        }
+
+        public void Connect(string userName, string password)
+        { 
+            _credentials = new UsernamePasswordCredentials { Username = userName, Password = password };
+        }
+
+        /// <summary>
+        /// Clones a remote repository to a local folder
+        /// </summary>
+        /// <param name="remote">The url of the git repository</param>
+        /// <param name="localPath">The local path the clone to</param>
+        public void Clone(string remote, string localPath)
+        {
+            var co = new CloneOptions();
+            co.CredentialsProvider = (_url, _user, _cred) => _credentials;
+            Repository.Clone(remote, localPath, co);
+        }
+
+        ~Lib2GitHelper()
+        {
+
+        }
+    }
+}
---
+++ TNDStudios.Apps.GitScanner/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using TNDStudios.Apps.GitScanner.Helpers;
+
+namespace TNDStudios.Apps.GitScanner
+{
+    class Program
+    {
+        static IConfigurationRoot configuration;
+
+        static void Main(string[] args)
+        {
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<IConfiguration>(configuration)
+                .AddTransient<IGitHelper, Lib2GitHelper>()
+                .BuildServiceProvider();
+
+            string userName = configuration["Git::UserName"];
+            string password = configuration["Git::Password"];
+            String[] repositoriesToScan = { "https://github.com/TNDStudios/TNDStudios.Azure.FunctionApp.git" };
+
+            IGitHelper gitHelper = serviceProvider.GetRequiredService<IGitHelper>();
+            gitHelper.Connect(userName, password);
+
+            foreach(string repositoryUrl in repositoriesToScan)
+            {
+                gitHelper.Clone(repositoryUrl, @"c:\temp");
+            }
+        }
+    }
+}
